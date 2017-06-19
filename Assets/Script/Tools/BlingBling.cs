d2a4cc2833b3b90381f6e4a2a13f9411f8a12042
root <13096337086@163.com>
2017-06-19 17:19:05 +0800

    MainSceneUI-1-20170619
    主界面UI搭建的第一天....搭建了个灵活的scrollview

---
+++ Assets/Script/Tools/BlingBling.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class BlingBling : MonoBehaviour {
+
+	public Image bling;
+	// Use this for initialization
+	void Start () {
+	
+		bling.DOFade (0, 1.5f).SetLoops (-1, LoopType.Yoyo); 
+	}
+}
---
+++ Assets/Script/Tools/ScrollThreeView.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class ScrollThreeView : MonoBehaviour,IBeginDragHandler,IEndDragHandler {
+
+	private ScrollRect scrollRect;
+	private float[] levelArray = new float[]{ 0f, 0.5f, 1.0f };
+	private float targetHorizontalPosition = 0.5f;
+	float smothing = 8;
+	bool isDrag = false;
+
+	// Use this for initialization
+	void Start () {
+		scrollRect = GetComponent<ScrollRect> ();
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		float horizontalPosX = scrollRect.horizontalNormalizedPosition;
+		int index = 0;
+		float offset = Mathf.Abs (levelArray [index] - horizontalPosX);
+		for (int i = 1; i < levelArray.Length; i++) {
+			float offsetTemp = Mathf.Abs (levelArray [i] - horizontalPosX);
+			if (offsetTemp < offset) {
+				index = i;
+				offset = offsetTemp;
+			}
+		}
+//		scrollRect.horizontalNormalizedPosition = levelArray [index];
+		targetHorizontalPosition = levelArray [index];
+		isDrag = false;
+	}
+
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		isDrag = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (isDrag == false) {
+			scrollRect.horizontalNormalizedPosition = Mathf.Lerp (scrollRect.horizontalNormalizedPosition,
+				targetHorizontalPosition,
+				Time.deltaTime * smothing);
+		}
+	}
+}
